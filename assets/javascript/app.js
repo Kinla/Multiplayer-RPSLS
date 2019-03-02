84f7c9df2e20ea6532be20cfcd53e3d0c4141132
SharonC <sharon.s.c.chien@gmail.com>
2019-03-01 19:50:16 -0500

    m

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCEaNxmB52HVvZIjK7JXK8R-hPGL9wVLkk",
+    authDomain: "multiplayer-rps-3a3e3.firebaseapp.com",
+    databaseURL: "https://multiplayer-rps-3a3e3.firebaseio.com",
+    projectId: "multiplayer-rps-3a3e3",
+    storageBucket: "",
+    messagingSenderId: "396364813584"
+};
+
+firebase.initializeApp(config);
